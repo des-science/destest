08d27de5d7a71c2fb99c6814fa89df90d6ccab82
troxel <michael.a.troxel@gmail.com>
2018-04-12 12:09:32 -0400

    push

--- destest.py
+++ destest.py
@@ -651,7 +651,7 @@ class MetaCalib(Calibrator):
             eSm = np.sum(get_array(self.e1)[mask_[2]]*w[2])
             if mask is not None:
                 print len(get_array(self.e1)),len(mask_[1]),len(mask[1])
-                print np.max(mask_[1])
+                print np.max(mask_[1]),mask_[1],mask[1]
                 eSp = np.sum(get_array(self.e1)[mask_[1]][mask[1]]*w[1])
                 eSm = np.sum(get_array(self.e1)[mask_[2]][mask[2]]*w[2])
             Rs = eSp/ws[1] - eSm/ws[2]
