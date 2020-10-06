6c256c3d11caf7cba03f3a7e9184434fced2d59c
matroxel <michael.troxel@duke.edu>
2020-10-06 15:00:43 -0400

    typo fix

--- destest/destest.py
+++ destest/destest.py
@@ -731,10 +731,10 @@ class Calibrator(object):
                 Rg1 = self.selector.get_masked(get_array(self.Rg1),mask)
                 Rg2 = self.selector.get_masked(get_array(self.Rg2),mask)
                 if col == self.params['e'][0]:
-                    Rs2 = self.select_resp(self.params['e'][1],mask,w,ws)
+                    Rs1 = self.select_resp(self.params['e'][0],mask,w,ws)
                 else:
-                    Rs2 = self.select_resp(self.params['e'][0],mask,w,ws)
-                return ((Rg1+Rg2)/2.+(Rs+Rs2)/2.)*w[0]
+                    Rs2 = self.select_resp(self.params['e'][1],mask,w,ws)
+                return ((Rg1+Rg2)/2.+(Rs1+Rs2)/2.)*w[0]
             elif return_full:
                 return R,c,w_
             else:
